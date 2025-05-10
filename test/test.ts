bed7256c6090e46931247011e3c0c193b3cb2d59
Brandon Lehmann <brandonlehmann@gmail.com>
2025-05-10 19:59:14 -0400

    update dependencies

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2022, Brandon Lehmann <brandonlehmann@gmail.com>
+// Copyright (c) 2021-2025, Brandon Lehmann <brandonlehmann@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import * as assert from 'assert';
+import assert from 'assert';
 import { describe, it, before, after } from 'mocha';
 import LocalStorage from '../src';
 
